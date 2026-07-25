ba5981eda7377e981dfa7f5990f80afacb7b9263
Lucas <ljeronimodarocha@gmail.com>
2026-07-25 16:18:24 -0300

    feat(book,user): adiciona tratamento centralizado de exceções

---
+++ Book/src/main/java/com/example/bookapp/exception/GrpcExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.example.bookapp.exception;
+
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class GrpcExceptionHandler {
+
+    public static StatusRuntimeException handleException(Throwable ex) {
+        if (ex instanceof UsernameNotFoundException e) {
+            return new StatusRuntimeException(Status.NOT_FOUND.withDescription(e.getMessage()));
+        }
+        if (ex instanceof BadCredentialsException e) {
+            return new StatusRuntimeException(Status.UNAUTHENTICATED.withDescription("Invalid username or password"));
+        }
+        if (ex instanceof AuthenticationException e) {
+            return new StatusRuntimeException(Status.UNAUTHENTICATED.withDescription(e.getMessage()));
+        }
+        if (ex instanceof AccessDeniedException e) {
+            return new StatusRuntimeException(Status.PERMISSION_DENIED.withDescription("Access denied"));
+        }
+        if (ex instanceof IllegalArgumentException e) {
+            return new StatusRuntimeException(Status.INVALID_ARGUMENT.withDescription(e.getMessage()));
+        }
+        if(ex instanceof StatusRuntimeException e){
+            return e;
+        }
+        if (ex instanceof RuntimeException e) {
+            return new StatusRuntimeException(Status.INTERNAL.withDescription(e.getMessage()));
+        }
+        return new StatusRuntimeException(Status.INTERNAL.withDescription("An unexpected error occurred"));
+    }
+}
